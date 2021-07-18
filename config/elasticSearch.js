0792a8af9b6a6596a6c5ab4b52e011789afdeda2
AbDwivedi7 <abhishekdwivedi131@gmail.com>
2021-07-18 15:46:57 +0530

    static files and variabled file added

--- config/elasticSearch.js
+++ config/elasticSearch.js
@@ -1,7 +1,6 @@
 import { Client } from '@elastic/elasticsearch'
 import elasticVariable from '../elasticConfig.json'
 
-// const id = process.env.ELASTICCLOUDID
 const client = new Client({
     cloud:{
         id: elasticVariable.id,
